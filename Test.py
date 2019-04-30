f0e6d76edb6be8dd1f2b773d4658556e80c5d28d
HannaMazur <50152985+HannaMazur@users.noreply.github.com>
2019-04-30 12:51:22 -0700

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,12 @@
+#comment
+# Section 4: Lists
+
+job_applicants=["John", "Geoff", "Brady", "Thomas"]
+hights=[61,70,67,64,65]
+ints_and_strings=[1,2,3,'four','five']
+empy_list=[]
+
+print(job_applicants)
+def text(print_this):
+    print(print_this)
+text("hello")
